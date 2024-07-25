1a1836411a99adb349fb1eef8fe4279fa36a41e1
Pavel Popov <pashafizmat1@gmail.com>
2024-07-25 12:00:25 +0300

    asynchronous processing foundation

--- main.cpp
+++ main.cpp
@@ -1,24 +1,45 @@
 #include "crossroad-manager.h"
 #include "utils.h"
 #include <iostream>
+#include <thread>
 
 auto main(int argc, char** argv) -> int {
-  CrossroadManager crmgr(Stage(StageType::PEDESTRIAN,
-                               default_durations[StageType::PEDESTRIAN],
-                               "PEDESTRIAN STAGE",
-                               std::move(initialize_traffic_lights<PedestrianTrafficLight, 5, 8>())),
-                         Stage(StageType::VEHICLE,
-                               default_durations[StageType::VEHICLE],
-                               "NORTH-SOUTH VEHICULAR STAGE",
-                               std::move(initialize_traffic_lights<VehicleTrafficLight, 2, 2>())),
+//  CrossroadManager crmgr(Stage(StageType::PEDESTRIAN,
+//                               default_durations[StageType::PEDESTRIAN],
+//                               "PEDESTRIAN STAGE",
+//                               initialize_traffic_lights<PedestrianTrafficLight, 5, 8>()),
+//                         Stage(StageType::VEHICLE,
+//                               default_durations[StageType::VEHICLE],
+//                               "NORTH-SOUTH VEHICULAR STAGE",
+//                               initialize_traffic_lights<VehicleTrafficLight, 2, 2>()),
 
-                         Stage(StageType::VEHICLE,
-                               default_durations[StageType::VEHICLE],
-                               "WEST-EAST VEHICULAR STAGE",
-                               std::move(initialize_traffic_lights<VehicleTrafficLight, 1, 2>())));
+//                         Stage(StageType::VEHICLE,
+//                               default_durations[StageType::VEHICLE],
+//                               "WEST-EAST VEHICULAR STAGE",
+//                               initialize_traffic_lights<VehicleTrafficLight, 1, 2>()));
 
-  std::cout << "kek" << std::endl;
-  //crmgr.main_loop();
+  std::cout << "WTF" << std::endl;
+  boost::asio::io_context io_context;
+  auto tl = TrafficLight(1, io_context, Color::RED,
+                             Color::YELLOW,
+                             Color::GREEN);
+  auto tls = TrafficLight(2, io_context, Color::RED,
+                             Color::YELLOW,
+                             Color::GREEN);
+  auto tlt = TrafficLight(3, io_context, Color::RED,
+                             Color::YELLOW,
+                             Color::GREEN);
+  auto tlf = TrafficLight(4, io_context, Color::RED,
+                             Color::YELLOW,
+                             Color::GREEN);
+  std::thread io_thread([&io_context]() {
+          io_context.run();
+      });
+
+  std::this_thread::sleep_for(std::chrono::seconds(300));
+  io_context.stop();
+  io_thread.join();
+  std::cout << "kek2" << std::endl;
   return 0;
 }
 
--- traffic-light.h
+++ traffic-light.h
@@ -4,33 +4,77 @@
 #include "event.h"
 #include "circular-list.h"
 #include <queue>
+#include <boost/asio.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/bind/bind.hpp>
+#include <iostream>
 
 class TrafficLight {
 public:
   template <typename... Args>
-  TrafficLight(unsigned short id, Args&&... args)
+  TrafficLight(unsigned short id, boost::asio::io_context& io_context, Args&&... args)
     : _id(id),
+      _io_context(io_context),
+      _timer(io_context),
       _traffic_light_colors(std::forward<Args>(args)...),
-      _active_color(*_traffic_light_colors.begin()) {};
-  //TODO: Timer with sending an Event to an another ID
-  virtual ~TrafficLight() = default;
+      _active_color(*_traffic_light_colors.begin()) {
+    std::cout << "C-TOR CALL" << std::endl;
+    start_timer();
+  };
   Color get_active_color();
-protected:
+  virtual ~TrafficLight() = default;
+
+private:
+  void start_timer() {
+    std::cout << "STARTING TIMER" << std::endl;
+    _timer.expires_after(Duration(2));
+    _timer.async_wait([this](const boost::system::error_code& error) {
+        std::cout << "WAITING" << std::endl;
+        if (!error) {
+            process_events();
+            start_timer();
+          }
+      });
+  };
+  void test(){std::cout << "SUCCESSFULLY HANDLED FROM ID: " << _id << std::endl;};
+  void process_events() {
+    test();
+    while (!_event_queue.empty()) {
+      Event event = _event_queue.front();
+      _event_queue.pop();
+      handle_event(event);
+    }
+  };
+
+  void handle_event(const Event& event) {
+    std::cout << "SUCCESSFULLY HANDLED EVENT FROM TRAFFIC LIGHT" << _id << std::endl;
+  };
   const unsigned short _id;
   std::queue<Event> _event_queue;
   CircularList<Color> _traffic_light_colors;
   Color _active_color;
+  boost::asio::io_context& _io_context;
+  boost::asio::steady_timer _timer;
 };
 
 class PedestrianTrafficLight: public TrafficLight {
 public:
   template <typename... Args>
-  PedestrianTrafficLight(unsigned short id, Args&&... args) : TrafficLight(id, std::forward<Args>(args)...) {};
+  PedestrianTrafficLight(unsigned short id,
+                         boost::asio::io_context& io_context,
+                         Args&&... args) : TrafficLight(id,
+                                                        io_context,
+                                                        std::forward<Args>(args)...) {};
 };
 
 class VehicleTrafficLight: public TrafficLight {
 public:
   template <typename... Args>
-  VehicleTrafficLight(unsigned short id, Args&&... args) : TrafficLight(id, std::forward<Args>(args)...) {};
+  VehicleTrafficLight(unsigned short id,
+                      boost::asio::io_context& io_context,
+                      Args&&... args) : TrafficLight(id,
+                                                     io_context,
+                                                     std::forward<Args>(args)...) {};
 };
+
 #endif //TRAFFICLIGHT_H
